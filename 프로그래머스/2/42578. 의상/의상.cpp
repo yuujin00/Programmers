63ba4fee8eec600ed2b47dd39d03c12179df6cd3
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2023-12-04 09:53:29 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+    int answer = 1;
+    
+    map<string,int> M;
+    for(int i=0; i<clothes.size(); i++) M[clothes[i][1]]++;
+    
+    for(auto iter=M.begin(); iter != M.end(); iter++) answer *= iter->second +1;
+    
+    return answer-1;
+}
