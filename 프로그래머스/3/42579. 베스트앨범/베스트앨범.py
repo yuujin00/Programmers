8625f7644e8e7ee396135d869083dec0fe2382bd
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-08-27 00:21:00 +0900

    [level 3] Title: 베스트앨범, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,34 +1,24 @@
 def solution(genres, plays):
     answer = []
     
-    genre={} # 장르 : [재생횟수,인덱스]
-    play={} # 장르 : 총 play
+    play={} #{'classic': 1450, 'pop': 3100}
+    genre={} #{'classic': [[500, 0], [150, 2], [800, 3]], 'pop': [[600, 1], [2500, 4]]}
     
     for i in range (len(genres)) :
-        if genres[i] in play:
+        if genres[i] in play :
             play[genres[i]] += plays[i]
-        else:
+            genre[genres[i]].append([plays[i],i])
+        else :
             play[genres[i]] = plays[i]
-            
-        if genres[i] in genre:
-            genre[genres[i]].append([plays[i], i])
-        else:
-            genre[genres[i]] = [[plays[i], i]]
-    
-    #print(genre)
-    #print(play)
+            genre[genres[i]] = [[plays[i],i]]
     
     genrelist=sorted(play.items(),key=lambda x : x[1], reverse=True)
-    
-    #print(genrelist)
+    # ['pop', 'classic']
     
     for (g, p) in genrelist :
         # 재생횟수 내림차순
         genre[g] = sorted(genre[g],key=lambda x : x[0], reverse=True)
-    
-    print(genre)
-    
-    for (g, p) in genrelist :
         answer+=[j for (i,j) in genre[g][:2]]
+    
         
     return answer
