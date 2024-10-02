49bce32abdcd796e111b0528dd5ffe35bcc05c45
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-03 03:03:41 +0900

    [level 2] Title: 구명보트, Time: 8.85 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 8.80 ms
+메모리: 10.6 MB, 시간: 8.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 05일 05:25:47
+2024년 10월 03일 03:03:39
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -2,15 +2,14 @@
 
 def solution(people, limit):
     answer = 0
-    people.sort()
-    start = 0
-    end = len(people) -1
+    people.sort(reverse=True)
+    start, end = 0, len(people) -1
     
     while(start<=end):
         if(people[start]+people[end] <=limit):
             start +=1
             end -=1
         else:
-            end-=1
+            start +=1
         answer +=1
     return answer
