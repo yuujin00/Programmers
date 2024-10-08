176396e97d8fdbedbe419fee22f864d8a08e3ab1
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-08 13:53:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -0,0 +1,22 @@
+#[1차] 비밀지도
+
+def solution(n, arr1, arr2):
+    answer = []
+    arr3=[0]*len(arr1)
+    
+    for i in range(len(arr1)):
+        k=arr1[i]|arr2[i]
+        arr3[i]=format(k,'b').zfill(n)
+        
+    #print(arr3)
+    
+    for arr in arr3:
+        tmp=''
+        for i in arr:
+            if i=='1': 
+                tmp+='#'
+            else :
+                tmp+=' '
+        answer.append(tmp)
+        
+    return answer
