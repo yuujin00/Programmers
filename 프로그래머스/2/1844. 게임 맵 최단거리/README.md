8fd354c16152712f0e8edd768f57d6c44743d0ca
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-15 02:26:26 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 23.54 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 24.34 ms
+메모리: 10.3 MB, 시간: 23.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 04일 16:19:40
+2024년 10월 15일 02:26:24
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,41 +1,42 @@
 # 게임 맵 최단거리
 
 def solution(maps):
-    answer = 0
-    n = len(maps) # 행
-    m = len(maps[0]) # 열
+    n = len(maps)
+    m = len(maps[0])
     
-    # 상 하 좌 우
     dx=[-1,1,0,0]
     dy=[0,0,-1,1]
     
+    # 초기
     q=[]
     q.append((0,0))
     
     while len(q) > 0 :
-        map= q.pop(0)
+        map = q.pop(0)
+        #print(map,map[0],map[1])
         
         for i in range(4):
-            x = map[0] + dx[i]
-            y = map[1] + dy[i]
+            x= map[0] + dx[i]
+            y= map[1] + dy[i]
             #print(x,y)
-            # x,y 가 맵 벗어날 경우
-            if not (0 <= x < n) or not (0 <= y < m):
+            
+            if not (0<=x<n) or not (0<=y<m) :
                 continue
-            # x,y = [0,0] , maps[x,y] == 0 : 벽
+            
             if (x==0 and y==0) or maps[x][y]==0 :
                 continue
-            # 나머지
-            if maps[x][y]==1 :
+                
+            if maps[x][y]==1:
                 maps[x][y] += maps[map[0]][map[1]]
                 
                 q.append((x,y))
                 #print(q,maps)
-            else : 
-                maps[x][y] = min(maps[x][y],maps[map[0]][map[1]]+1)
-            #print(maps)
                 
-    if maps[n-1][m-1]==1 :
+            else : 
+                maps[x][y] = min(maps[x][y], maps[map[0]][map[1]]+1)
+            #print(maps)
+        
+    if maps[n-1][m-1]==1:
         return -1
-    
+        
     return maps[n-1][m-1]
