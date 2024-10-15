4098609150cd8d2509f009cb285431294ef029ed
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-16 02:59:33 +0900

    [level 1] Title: 명예의 전당 (1), Time: 4.74 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/README.md
+++ 프로그래머스/1/138477. 명예의 전당 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 1] 명예의 전당 (1) - 138477 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138477) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 6.21 ms
+메모리: 10.3 MB, 시간: 4.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 08일 14:52:16
+2024년 10월 16일 02:59:31
 
 ### 문제 설명
 
--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -1,12 +1,14 @@
 def solution(k, score):
     answer = []
-    R3=[]
+    Round=[]
+    
     for i in score:
-        if len(R3)<k:
-            R3.append(i)
-        else:
-            if min(R3)<i:
-                R3.remove(min(R3))
-                R3.append(i)
-        answer.append(min(R3))
+        if len(Round)<k:
+            Round.append(i)
+        else :
+            if min(Round)<i:
+                Round.remove(min(Round))
+                Round.append(i)
+        answer.append(min(Round))
+    
     return answer
