4c06f12a218f998056dfb94b3cb456ca6fabdf4a
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-17 23:17:56 +0900

    [level 2] Title: 올바른 괄호, Time: 7.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 7.09 ms
+메모리: 10.2 MB, 시간: 7.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 12일 02:22:51
+2024년 10월 17일 23:17:54
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,12 +1,16 @@
 def solution(s):
     answer = True
     stack=[]
-    for i in s : 
-        if(i=='(') : stack.append(i)
-        else : 
-            if(stack==[]) : answer=False
-            else : stack.pop()
     
-    if(stack!=[]) : answer=False
-    
+    for i in s :
+        if i=='(':
+            stack.append(i)
+        else:
+            if stack==[] :
+                answer=False
+            else : 
+                stack.pop()
+            
+    if stack!=[] : answer=False
+
     return answer
