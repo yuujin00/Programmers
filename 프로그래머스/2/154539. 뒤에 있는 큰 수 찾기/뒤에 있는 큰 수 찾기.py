497048561ef6eb67de6e17864bd97d8ca70817c8
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-22 20:46:25 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 297.80 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.py
@@ -1,15 +1,11 @@
 # 뒤에 있는 큰 수 찾기
 
 def solution(numbers):
-    answer = [-1] * len(numbers)
-    stack =[]
+    answer = [-1]*len(numbers)
+    stack=[]
     
     for i in range(len(numbers)):
-            while stack and numbers[stack[-1]] < numbers[i]:
-                #print(numbers[stack[-1]],'<',numbers[i])
-                answer[stack.pop()] = numbers[i]
-                #print(answer)
-            stack.append(i)
-            #print(stack)
-    
+        while stack and numbers[stack[-1]] <numbers[i] :
+                answer[stack.pop()]=numbers[i]
+        stack.append(i)
     return answer
