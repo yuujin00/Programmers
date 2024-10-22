7bf316f81298eadb11204b84ad994a431c45c372
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-22 21:00:12 +0900

    [level 2] Title: 주식가격, Time: 149.37 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,12 +1,14 @@
 # 주식가격
 
 def solution(prices):
-    answer = []
+    answer = [0]*len(prices)
     
-    for i in range (len(prices)) :
-        n=0
-        for j in range (i+1,len(prices)) :
-            n+=1
-            if (prices[i] > prices[j]) : break
-        answer.append(n)
+    for i in range(len(prices)-1):
+        cnt=0
+        for j in range(i+1,len(prices)):
+            cnt+=1
+            if prices[i]>prices[j]:
+                break
+        answer[i]=cnt
+        
     return answer
