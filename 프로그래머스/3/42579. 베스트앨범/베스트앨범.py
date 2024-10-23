d1c692206e16d2a6bc37e451d73d232d90f40b65
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2024-10-23 22:17:22 +0900

    [level 3] Title: 베스트앨범, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -4,24 +4,25 @@ def solution(genres, plays):
     play={}
     genre={}
     
-    for i in range(len(genres)):
+    for i in range(len(genres)) :
         if genres[i] in play :
             play[genres[i]] += plays[i]
         else :
             play[genres[i]]=plays[i]
-        if genres[i] in genre:
+        
+        if genres[i] in genre :
             genre[genres[i]].append([plays[i],i])
         else :
             genre[genres[i]]=[[plays[i],i]]
-            
+        
     #print(play,genre)
-     
-    genre_list=sorted(play.items(), key=lambda x : x[1], reverse=True)
+    
+    genre_list = sorted(play.items(), key=lambda x : x[1], reverse =True)
     
     #print(genre_list)
     
-    for g,p in genre_list:
-        genre[g]=sorted(genre[g],key=lambda x : x[0], reverse =True)
+    for g,p in genre_list :
+        genre[g]=sorted(genre[g],key=lambda x : x[0], reverse=True)
         #print(genre[g])
         
         for i,j in genre[g][:2]:
