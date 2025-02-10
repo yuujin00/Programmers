786f7d5d97282334e2520e83fa9b440cf5bc42b3
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2025-02-10 22:16:16 +0900

    [level 2] Title: 소수 찾기, Time: 21.92 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 6.20 ms
+메모리: 10.4 MB, 시간: 21.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 16:41:31
+2025년 02월 10일 22:16:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,24 +1,21 @@
-# 소수 찾기
-
 from itertools import permutations
 
-def check(n):
-    if n<2 :
+def  check(n):
+    if n<2:
         return False
     for i in range(2,int(n**0.5)+1):
         if n % i == 0 :
             return False
-        
+            
     return True
 
 def solution(numbers):
     answer = set()
     
-    for i in range(1, len(numbers)+1) :
-        number=set(map(int,map("".join, permutations(numbers, i))))
-        print(number)
-        for j in number :
-            if check(j) :
-                answer.add(j)
-                
+    
+    for i in range(1,len(numbers)+1):
+        for pe in permutations(numbers,i):
+            if check(int(''.join(pe))):
+                answer.add(int(''.join(pe)))
+    
     return len(answer)
