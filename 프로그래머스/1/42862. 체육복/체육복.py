865d92d3c5495bc0d5927a401337c03945e084da
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2025-02-16 23:59:34 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,23 +1,22 @@
-# 체육복
-
 def solution(n, lost, reserve):
-    answer = 0 
-    lost.sort()
-    reserve.sort()
-	
-    # 공통 제거
-    for i in reserve[:]:
-        if i in lost:
-            reserve.remove(i)
-            lost.remove(i)
+    answer = 0
+    people=[1 for i in range(n)]
+    # 도난
+    for i in lost :
+        people[i-1]-=1
+    # 여벌
+    for i in reserve :
+        people[i-1]+=1
+    #print(people)
     
-    for i in reserve: 
-        if i-1 in lost: 
-            lost.remove(i-1) 
-            
-        elif i+1 in lost: 
-            lost.remove(i+1) 
-            
-    answer = n - len(lost)
+    # 빌리기
+    for i in range(len(people)):
+        if people[i]==0:
+            if i>0 and people[i-1]==2:
+                people[i]=1
+                people[i-1]=1
+            elif i+1<n and people[i+1]==2:
+                people[i]=1
+                people[i+1]=1
     
-    return answer
+    return n-people.count(0)
