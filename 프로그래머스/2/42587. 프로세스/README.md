40a2d74a64ad1dca60fb8ff39b390da56fc794f4
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2025-02-27 17:37:26 +0900

    [level 2] Title: 프로세스, Time: 0.70 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.89 ms
+메모리: 10.2 MB, 시간: 0.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 25일 15:55:28
+2025년 02월 27일 17:37:23
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,21 +1,21 @@
-# 프로세스
-
 from collections import deque
 
 def solution(priorities, location):
     answer = 0
     p=deque(priorities)
     
-    while len(p)>0 :
+    while p :
         M=max(p)
         tmp=p.popleft()
         location-=1
         
-        if tmp != M :
+        if tmp != M:
             p.append(tmp)
-            if location == -1 :
-                location = len(p)-1
+            if location==-1:
+                location=len(p)-1
+                
         else :
             answer+=1
-            if location == -1: break
+            if location==-1 : break
+    
     return answer
