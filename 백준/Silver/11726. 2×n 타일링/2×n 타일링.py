a4b8282ea5a1a8888ef6b0bf02a1491575ad5ce9
yuujin00 <146426960+yuujin00@users.noreply.github.com>
2025-03-20 00:32:25 +0900

    [Silver III] Title: 2×n 타일링, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -1,13 +1,11 @@
-n = int(input())
+N= int(input())
+
 dp = [0,1,2]
 
-
-
-if n <= 2:
-    print(dp[n])
-else:
-    for i in range(3,n+1):
-        dp.append((dp[i-1] + dp[i-2])  % 10007)
-        #print(i,dp[i])
-
-    print(dp[n])
+if N <=2:
+    print(dp[N])
+else :
+    for i in range(3,N+1):
+        dp.append((dp[i-1]+dp[i-2])  % 10007)
+        #print(i, dp[i])
+    print(dp[N])
